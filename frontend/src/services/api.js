7cf7d20f4d7eabf02445ec6911b73602d3ad2a33
serize <shja0831@gmail.com>
2025-12-29 00:14:02 +0900

    .

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000/api'
+const API_URL = 'https://duskish-enda-unacetic.ngrok-free.dev/api'
 
 const api = axios.create({
   baseURL: API_URL,
